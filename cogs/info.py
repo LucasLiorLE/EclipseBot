8261793a9c9d9029b651f76a7221519d12070c09
LucasLiorLE <97490067+LucasLiorLE@users.noreply.github.com>
2024-04-15 19:09:54 -0700

    Update info.py
    Deleted uneeded stuff

--- cogs/info.py
+++ cogs/info.py
@@ -1,127 +1,234 @@
-from discord.ext import commands
-import discord
-import json 
 import os
+import json
+from discord.ext import commands, tasks
+import discord
+import datetime
+import aiohttp
+import re
+import humanize
+import asyncio
+from googletrans import Translator, LANGUAGES
 
-def load_help_data():
-    dir_path = os.path.dirname(os.path.abspath(__file__))
-    help_json_path = os.path.join(dir_path, '..', 'Storage', 'help.json')
-    
-    with open(help_json_path, 'r') as file:
-        return json.load(file)
-
-help_data = load_help_data()
-
-class HelpSelect(discord.ui.Select):
-    def __init__(self):
-        options = [
-            discord.SelectOption(label="Moderation", emoji="📋", description="Displays all the commands for moderation"),
-            discord.SelectOption(label="Utilities", emoji="✨", description="Displays all the commands for utilities"),
-            discord.SelectOption(label="Information", emoji="🗣️", description="Shows all the commands for information"),
-            discord.SelectOption(label="Fun", emoji="😃", description="Shows all the commands for fun")
-        ]
-        super().__init__(placeholder="Select a category", max_values=1, min_values=1, options=options)
-
-    async def callback(self, interaction: discord.Interaction):
-        category = self.values[0]
-        commands = help_data.get(category, {})
-        
-        description = "\n".join([f"`{cmd}` - {desc}" for cmd, desc in commands.items()])
-        
-        embed = discord.Embed(title=f"{category.capitalize()} Commands", description=description, color=discord.Color.blue())
-        embed.set_footer(text="(optional args) {required args}")
-        
-        await interaction.response.send_message(embed=embed, ephemeral=True)
-
-
-class SelectView(discord.ui.View):
-    def __init__(self):
-        super().__init__()
-        self.add_item(HelpSelect())
-
-class Info(commands.Cog):
+class Utilities(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.reminders = []
+
 
     @commands.command()
-    async def ping(self, ctx):
-        latency = round(self.bot.latency * 1000)
-        color = (0x00FF00 if latency < 81 else 
-                 0xFFFF00 if latency < 201 else 
-                 0xFF0000)
-        embed = discord.Embed(title="Pong!", description=f"The latency is {latency}ms.", color=color)
-        await ctx.send(embed=embed)
-
-    @commands.command()
-    async def info(self, ctx):
-        embed = discord.Embed(title="Bot Info", description="This bot is developed by LucasLiorLE.", color=0x808080)
-        embed.add_field(name="Version", value="1.42.6")
-        embed.add_field(name="Source Code", value="[GitHub Repository](https://github.com/LucasLiorLE/EclipseBot)")
-        embed.set_footer(text=f"Requested by {ctx.author}", icon_url=ctx.author.avatar.url)
-        await ctx.send(embed=embed)
-
-    @commands.command()
-    async def serverinfo(self, ctx):
-        guild = ctx.guild
-        embed = discord.Embed(title="Server Info", color=0x808080)
-        embed.add_field(name="Server Name", value=guild.name)
-        embed.add_field(name="Server Owner", value=guild.owner.mention)
-        embed.add_field(name="Server ID", value=guild.id)
-        embed.add_field(name="Members", value=guild.member_count)
-        embed.set_footer(text=f"Requested by {ctx.author}", icon_url=ctx.author.avatar.url)
-        await ctx.send(embed=embed)
-
-    @commands.command()
-    async def userinfo(self, ctx, member: discord.Member = None):
-        member = member or ctx.author
-        roles = [role.name for role in member.roles]
-        embed = discord.Embed(title="User Info", color=0x808080)
-        embed.set_thumbnail(url=member.avatar.url)
-        embed.add_field(name="Username", value=member.display_name)
-        embed.add_field(name="User ID", value=member.id)
-        embed.add_field(name="Joined Discord", value=member.created_at.strftime("%b %d, %Y"))
-        embed.add_field(name="Joined Server", value=member.joined_at.strftime("%b %d, %Y"))
-        embed.add_field(name="Roles", value=', '.join(roles))
-        embed.set_footer(text=f"Requested by {ctx.author}", icon_url=ctx.author.avatar.url)
-        await ctx.send(embed=embed)
-
-    @commands.command()
-    async def avatar(self, ctx, member: discord.Member = None):
-        member = member or ctx.author
-        embed = discord.Embed(title=f"{member.display_name}'s Avatar", color=0x808080)
-        embed.set_image(url=member.avatar.url)
-        embed.set_footer(text=f"Requested by {ctx.author}", icon_url=ctx.author.avatar.url)
-        await ctx.send(embed=embed)
-
-    @commands.command()
-    async def help(self, ctx, *, command_name: str = None):
-        if command_name:
-            command_info = search_command(command_name.lower())
-            if not command_info:
-                await ctx.send(f"No such command named `{command_name}`.")
+    async def cgloves(self, ctx, roblox_username: str):
+        a = await ctx.reply("Fetching user gloves. This might take a while...")
+        async with aiohttp.ClientSession() as session:
+            response = await session.post("https://users.roblox.com/v1/usernames/users", json={"usernames": [roblox_username], "excludeBannedUsers": True})
+            if response.status != 200:
+                await a.edit("An error occurred while fetching the user's Roblox ID.")
                 return
-            category = list(command_info.keys())[0]
-            matched_cmd = list(command_info[category].keys())[0]  
-            command_description = command_info[category][matched_cmd]
+            data = await response.json()
+            if not data["data"]:
+                await a.edit(f"No data found for the username: {roblox_username}")
+                return
+            roblox_id = data["data"][0]["id"]
+
+            file_path = os.path.join(os.path.dirname(__file__), '..', 'Storage', 'gloves.json')
+
+            with open(file_path, 'r') as f:
+                gloves = json.load(f)
+
+            all_badge_ids = [badge_id for badge_ids in gloves.values() for badge_id in badge_ids]
+            url = f"https://badges.roblox.com/v1/users/{roblox_id}/badges/awarded-dates?badgeIds={','.join(map(str, all_badge_ids))}"
+            response = await session.get(url)
+
+            if response.status == 200:
+                data = await response.json()
+                if not data['data']:
+                    await a.edit(f"No badges found for the user: {roblox_username}")
+                    return
+
+                owned = [glove for glove, badge_ids in gloves.items() if all(any(badge.get('badgeId') == badge_id for badge in data['data']) for badge_id in badge_ids)]
+                not_owned = [glove for glove in gloves.keys() if glove not in owned]
+
+                additional_owned = {}
+                for badge_name, badge_id in {"Welcome": 2124743766, "Met Owner": 2124760252, "Met Snow": 2124760875, "Clipped Wings": 2147535393}.items():
+                    url = f"https://badges.roblox.com/v1/users/{roblox_id}/badges/awarded-dates?badgeIds={badge_id}"
+                    response = await session.get(url)
+                    if response.status == 200:
+                        data = await response.json()
+                        if data['data']:
+                            additional_owned[badge_name] = data['data'][0]['awardedDate']
+
+                embed = discord.Embed(title=f"SB Data for {roblox_username}:{roblox_id}:", description=f"Badge gloves:\n{len(owned)}/{len(gloves)} badge gloves owned.", color=0xFFA500)
+                embed.add_field(name="OWNED", value=', '.join(owned) if owned else 'None', inline=False)
+                embed.add_field(name="NOT OWNED", value=', '.join(not_owned) if not_owned else 'None', inline=False)
+
+                for badge_name, awarded_date in additional_owned.items():
+                    date, time, fraction = awarded_date.replace('Z', '+0000').partition('.')
+                    fraction = fraction[:fraction.index('+')][:6] + '+0000'
+                    awarded_date = f"{date}.{fraction}"
+                    awarded_date = datetime.datetime.strptime(awarded_date, "%Y-%m-%dT%H:%M:%S.%f%z")
+                    epoch_time = int(awarded_date.timestamp())
+                    embed.add_field(name=badge_name, value=f"Obtained on <t:{epoch_time}:F>", inline=False)
+
+                await a.edit(embed=embed)
+            else:
+                await a.edit("An error occurred while fetching the user's badges.")
+
+
+    @commands.command()
+    @commands.cooldown(1, 5, commands.BucketType.user)
+    async def convert(self, ctx, value: float, unit_from: str, unit_to: str):
+        
+        length_factors = {'mm': 0.001, 'cm': 0.01, 'm': 1.0, 'km': 1000.0}
+        weight_factors = {'mg': 0.001, 'g': 1.0, 'kg': 1000.0, 't': 1000000.0}
+        volume_factors = {'ml': 0.001, 'l': 1.0, 'kl': 1000.0}
+        
+        temperature_factors = {
+            'C': {'F': lambda c: c * 9/5 + 32, 'K': lambda c: c + 273.15},
+            'F': {'C': lambda f: (f - 32) * 5/9, 'K': lambda f: (f - 32) * 5/9 + 273.15},
+            'K': {'C': lambda k: k - 273.15, 'F': lambda k: (k - 273.15) * 9/5 + 32}
+        }
+
+        if unit_from in length_factors and unit_to in length_factors:
+            converted_value = value * length_factors[unit_from] / length_factors[unit_to]
+            await ctx.reply(f"{value} {unit_from} is {converted_value} {unit_to}.")
+        
+        elif unit_from in weight_factors and unit_to in weight_factors:
+            converted_value = value * weight_factors[unit_from] / weight_factors[unit_to]
+            await ctx.reply(f"{value} {unit_from} is {converted_value} {unit_to}.")
+        
+        elif unit_from in volume_factors and unit_to in volume_factors:
+            converted_value = value * volume_factors[unit_from] / volume_factors[unit_to]
+            await ctx.reply(f"{value} {unit_from} is {converted_value} {unit_to}.")
+        
+        elif unit_from in temperature_factors and unit_to in temperature_factors[unit_from]:
+            converted_value = temperature_factors[unit_from][unit_to](value)
+            await ctx.reply(f"{value}°{unit_from} is {converted_value}°{unit_to}.")
+        
+        else:
+            await ctx.reply("Invalid units or incompatible unit types. Please check your units and try again.")
+
+
+    @commands.command()
+    @commands.cooldown(1, 5, commands.BucketType.user)
+    async def remind(self, ctx, duration, *, reason=None):
+        time = self.parse_duration(duration)
+        
+        if time is None:
+            await ctx.reply("Invalid duration format. Use `1h`, `1m`, `1s`, or `1d`.")
+            return
+        
+        if reason is None:
+            reason = "No reason provided"
+
+        reminder = {
+            "user": ctx.author.id,
+            "channel": ctx.channel.id,
+            "reason": reason,
+            "end_time": ctx.message.created_at + time
+        }
+        
+        self.reminders.append(reminder)
+        
+        await ctx.reply(f"Reminder set for {humanize.naturaldelta(time.total_seconds())} from now.", delete_after=3)
+        
+        await asyncio.sleep(time.total_seconds())
+        await ctx.reply(f"{ctx.author.mention}, your reminder for '{reason}' is due!", delete_after=10)
+        self.reminders.remove(reminder)
+        
+    def parse_duration(self, duration):
+        match = re.match(r"(\d+)([hmsd])", duration)
+        if match:
+            amount, unit = match.groups()
+            amount = int(amount)
             
-            embed = discord.Embed(title=f"Help for `{matched_cmd}`", color=discord.Color.blue())
-            embed.add_field(name="Description", value=command_description)
-            embed.add_field(name="Category", value=category)
-            embed.set_footer(text="(optional args) {required args}")
-            await ctx.send(embed=embed)
+            if unit == "h":
+                return datetime.timedelta(hours=amount)
+            elif unit == "m":
+                return datetime.timedelta(minutes=amount)
+            elif unit == "s":
+                return datetime.timedelta(seconds=amount)
+            elif unit == "d":
+                return datetime.timedelta(days=amount)
+        
+        return None
+
+    @tasks.loop(minutes=1)
+    async def check_reminders(self):
+        now = datetime.datetime.utcnow()
+        for reminder in self.reminders:
+            if now >= reminder["end_time"]:
+                channel = self.bot.get_channel(reminder["channel"])
+                user = self.bot.get_user(reminder["user"])
+                await channel.reply(f"Hey {user.mention}, reminder for {reminder['reason']}!")
+
+    @commands.command()
+    @commands.cooldown(1, 5, commands.BucketType.user)
+    async def translate(self, ctx, *, input_str: str = None):        
+        translator = Translator()
+        
+        if not input_str:
+            await ctx.reply("Please provide a target language and text to translate separated by '|'.")
+            return
+        
+        parts = input_str.split('|')
+        
+        if len(parts) != 2:
+            await ctx.reply("Invalid format. Please use 'target_lang | text' format.")
+            return
+        
+        target_lang = parts[0].strip().lower()
+        text = parts[1].strip()
+        
+        if target_lang == 'chineses':
+            target_lang = 'chinese (simplified)'
+        elif target_lang == 'chineset':
+            target_lang = 'chinese (traditional)'
+        elif target_lang == 'chinese':
+            await ctx.reply('Please enter chineset or chineses next time.\nI believe you want simplified')
+            target_lang = 'chinese (simplified)'
+        
+        if target_lang in LANGUAGES:
+            target_lang_code = target_lang
+        elif target_lang.lower() in [v.lower() for v in LANGUAGES.values()]:
+            target_lang_code = next(k for k, v in LANGUAGES.items() if v.lower() == target_lang.lower())
+        else:
+            await ctx.reply("Invalid language. Please use a valid language name or language code.")
             return
 
-        view = SelectView()
-        await ctx.send("Please select a category:", view=view)
 
-# END
+        try:
+            translated = translator.translate(text, dest=target_lang_code)
+            translated_text = translated.text
+            detected_lang = translated.src
+            
+            embed = discord.Embed(title="Translation", color=discord.Color.gold())
+            embed.add_field(name="Original Text", value=f"`{text}`", inline=False)
+            embed.add_field(name="Detected Language", value=detected_lang, inline=False)
+            embed.add_field(name="Translated Text", value=f"`{translated_text}`", inline=False)
+            embed.set_footer(text=f"Translated to {LANGUAGES[target_lang_code].capitalize()}")
+            
+            await ctx.reply(embed=embed)
+            
+        except Exception as e:
+            await ctx.reply(f"An error occurred: {e}")
 
-def search_command(command_name):
-    for category, commands in help_data.items():
-        for cmd, description in commands.items():
-            if cmd.startswith(command_name):
-                return {category: {cmd: description}}
-    return None
+    @commands.command()
+    @commands.has_permissions(administrator=True)
+    async def prefix(self, ctx, prefix: str):
+        file_path = os.path.join(os.path.dirname(__file__), '..', 'Storage', 'preferences.json')
+        
+        with open(file_path, 'r') as f:
+            data = json.load(f)
+        
+        guild_id = str(ctx.guild.id) 
+        data[guild_id] = {"prefix": prefix} 
+        
+        with open(file_path, 'w') as f:
+            json.dump(data, f, indent=4)
+        
+        self.bot.command_prefix = commands.when_mentioned_or(prefix)
+        
+        await ctx.reply(f"Bot prefix updated to `{prefix}`.")
+
 
 def setup(bot):
-    bot.add_cog(Info(bot))
+    bot.add_cog(Utilities(bot))
